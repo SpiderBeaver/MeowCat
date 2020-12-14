6fee26bc7a8e889c78da3fa624b83659b5c0124f
Spider-Beaver <spiderbeaver@gmail.com>
2020-12-14 17:12:50 +0300

    Try data fetching

--- MeowCat_client/src/App.tsx
+++ MeowCat_client/src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Post from './domain/Post';
 
 function App() {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('http://localhost:8000/posts');
+      const data = await response.json();
+      setPosts(data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.text}</li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ MeowCat_client/src/domain/Post.ts
@@ -0,0 +1,4 @@
+export default class Post {
+  id!: number;
+  text!: string;
+}
